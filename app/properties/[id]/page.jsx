5dae49df826585d18d34ad60c8c5eb74253e7b32
Becki Wordsworth <63713673+BeckiWordsworth@users.noreply.github.com>
2024-06-24 23:05:10 +0200

    Playing around with params

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -1,7 +1,21 @@
+"use client";
+
 import React from "react";
+import { useRouter, useParams, useSearchParams } from "next/navigation";
 
 const PropertyPage = () => {
-  return <div>PropertyPage</div>;
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const { id } = useParams();
+  const name = searchParams.get("name");
+
+  return (
+    <div>
+      <button onClick={() => router.push("/")} className="bg-blue-500 p-2">
+        Go Home {name}
+      </button>
+    </div>
+  );
 };
 
 export default PropertyPage;
