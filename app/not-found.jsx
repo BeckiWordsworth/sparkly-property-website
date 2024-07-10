3226decc55b59c4758aae5a5088cf8db9d995c23
Becki Wordsworth <63713673+BeckiWordsworth@users.noreply.github.com>
2024-07-10 22:28:19 +0200

    Fixing links

--- app/not-found.jsx
+++ app/not-found.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Link from "next/link";
 import { FaExclamationTriangle } from "react-icons/fa";
 
 const NotFoundPage = () => {
@@ -12,9 +13,9 @@ const NotFoundPage = () => {
           <div className="text-center">
             <h1 className="text-3xl font-bold mt-4 mb-2">Page Not Found</h1>
             <p className="text-gray-500 text-xl mb-10">The page you are looking for does not exist.</p>
-            <a href="/index.html" class="bg-blue-700 hover:bg-blue-800 text-white font-bold py-4 px-6 rounded">
+            <Link href="/index.html" class="bg-blue-700 hover:bg-blue-800 text-white font-bold py-4 px-6 rounded">
               Go Home
-            </a>
+            </Link>
           </div>
         </div>
       </div>
